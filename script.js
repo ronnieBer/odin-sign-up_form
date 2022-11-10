b1c3e2af714d5b6778576894c6fda13dbc11eb37
Ronnie <ronniebercero@gmail.com>
2022-11-10 23:56:03 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const email = document.querySelector('#email');
+const phoneNumber = document.querySelector('#phone-number');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password');
+
+const errFName = document.querySelector('#err-f-name');
+const errLName = document.querySelector('#err-l-name');
+const errEmail = document.querySelector('#err-email');
+const errPNum = document.querySelector('#err-p-num');
+const errPw = document.querySelector('#err-pw');
+const errConfirmPw =document.querySelector('#err-confirm-pw');
