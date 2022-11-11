b85bc517c1af76935adca7590f17b40c6d44a74f
Ronnie <ronniebercero@gmail.com>
2022-11-11 23:38:13 +0800

    Add event listener to first name input

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ const errEmail = document.querySelector('#err-email');
 const errPNum = document.querySelector('#err-p-num');
 const errPw = document.querySelector('#err-pw');
 const errConfirmPw =document.querySelector('#err-confirm-pw');
+
+firstName.addEventListener('input', (e) => {
+
+    if (firstName.ariaValueMax.length == 0) {
+        errFName.innerText = 'First name is required.';
+        firstName.style.border = '2px solid #B91C1C';
+        return false;
+    }
+
+    if (!firstName.ariaValueMax.match(/^[A-Za-z]*$/)) {
+        errFName.innerText = 'Alpha letters only';
+        firstName.style.border = '2px solid #B91C1C';
+        return false;
+    }
+
+    errFName.innerText = '';
+    firstName.style.border = '2px solid #E5E7EB';
+    return true;
+
+});
