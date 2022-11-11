bbdde46d8bc66b3c3379fdc934d632b867f9dced
Ronnie <ronniebercero@gmail.com>
2022-11-11 23:42:52 +0800

    Change ariaValueMax to value

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ const errConfirmPw =document.querySelector('#err-confirm-pw');
 
 firstName.addEventListener('input', (e) => {
 
-    if (firstName.ariaValueMax.length == 0) {
+    if (firstName.value.length == 0) {
         errFName.innerText = 'First name is required.';
         firstName.style.border = '2px solid #B91C1C';
         return false;
     }
 
-    if (!firstName.ariaValueMax.match(/^[A-Za-z]*$/)) {
+    if (!firstName.value.match(/^[A-Za-z]*$/)) {
         errFName.innerText = 'Alpha letters only';
         firstName.style.border = '2px solid #B91C1C';
         return false;
